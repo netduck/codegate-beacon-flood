0479c26a0766e9d9a8bc2f2daf202a9fbc592802
Aiden <aidencom99@gmail.com>
2022-10-30 14:03:07 -0400

    Split Directory

--- backend/c/fake_ap.c
+++ backend/c/fake_ap.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "beacon.h"
+#include "utils/beacon.h"
 
 #define MAX_SIZE 10
 
---
+++ backend/c/utils/request.h
@@ -0,0 +1,11 @@
+#pragma once
+#define PORT            1235
+#define LOCAL_HOST      "127.0.0.1"
+#define SSID_LEN        32
+#define QUEUE_SIZE      10
+
+using namespace std;
+extern queue<char*> request_queue;
+
+int sock_init(void);
+void read_ssid_from_client(void);
